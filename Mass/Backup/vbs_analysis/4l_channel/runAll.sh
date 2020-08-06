e390a01cada8d478b36f344d89f76fa59a44955f
lolivi <65222151+lolivi@users.noreply.github.com>
2020-08-06 11:19:24 +0200

    Update runAll.sh

--- Mass/Backup/vbs_analysis/4l_channel/runAll.sh
+++ Mass/Backup/vbs_analysis/4l_channel/runAll.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 fail_exit() { echo "$@" 1>&2; exit 1; } 
 
-cd /<path>/CMSSW_8_0_26_patch1/src/vbs_analysis/4l_channel/
+cd /<path>/CMSSW_8_0_26_patch1/src/Mass/vbs_analysis/4l_channel/
 export SCRAM_ARCH=slc7_amd64_gcc530
 
 echo "Start of job on " `date`
