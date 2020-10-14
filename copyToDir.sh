87daba1d40e1c7107f5d04b7e34ea29ad9632c54
lolivi <65222151+lolivi@users.noreply.github.com>
2020-10-14 18:01:39 +0200

    Update copyToDir.sh

--- copyToDir.sh
+++ copyToDir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {0..200}
+for i in {0..2000}
 do
   echo "cp $1_results/$2/$1_$i/unweighted_events.lhe /<path>/$3/unweighted_events_$i.lhe"
   cp $1_results/$2/$1_$i/unweighted_events.lhe /<path>/$3/unweighted_events_$i.lhe
