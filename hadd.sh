434017efec70df314e3318cb0868f9ced9e0dd00
lolivi <65222151+lolivi@users.noreply.github.com>
2020-10-17 17:14:56 +0200

    Add files via upload

--- hadd.sh
+++ hadd.sh
@@ -2,7 +2,7 @@ for chan in 2e2mu 4mu 4e
 do
     for year in 2016 2017 2018
     do
-	hadd -f tot_1D_vbs4lSyst_"$chan"1S_"$year".input_func.root 1D_vbs4lSyst_"$chan"1S_"$year".input_func.root lin_1D_vbs4lSyst_"$chan"1S_"$year".input_func.root quad_1D_vbs4lSyst_"$chan"1S_"$year".input_func.root
+	hadd -f tot_1D_vbs4lSyst_"$chan"1S_"$year".input_func.root 1D_vbs4lSyst_"$chan"1S_"$year".input_func.root newlin_1D_vbs4lSyst_"$chan"1S_"$year".input_func.root newquad_1D_vbs4lSyst_"$chan"1S_"$year".input_func.root
     done
 done
 
---
+++ runnewhisto.sh
@@ -0,0 +1,8 @@
+for chan in 2e2mu 4mu 4e
+	do
+	for year in 2016 2017 2018 
+		do
+			./newhisto.lsf "$chan" $year
+			done
+			done
+
